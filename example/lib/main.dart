35f3b14f0e6072ccd30db3712ece98fd85d21151
Rishit Dagli <rishit.dagli@gmail.com>
2020-02-16 05:48:14 +0000

    Added example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,52 @@
+import 'package:flutter/material.dart';
 import 'package:popup_box/popup_box.dart';
 
-FloatingActionButton(
-  onPressed: () async {
-  await CustomAlertBox.showCustomAlertBox(
-context: context,
-willDisplayWidget: Container(
-child: Text('My custom alert box, used from example!!'),
-));
-},
-tooltip: 'Show Custom Alert Box',
-child: Icon(Icons.message),
-)
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'PopupBox Package'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+            'To show a popup box,e click floating action button.'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await PopupBox.showPopupBox(
+              context: context,
+              willDisplayWidget: Container(
+                child: Text('An example popup box text'),
+              ));
+        },
+        tooltip: 'Show Popup Box Box',
+        child: Icon(Icons.message),
+      ),
+    );
+  }
+}
